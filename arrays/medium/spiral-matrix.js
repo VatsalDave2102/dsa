586b0ca2b6bf6fdab84364ac525b15935cb00152
Vatsal Dave <vatsaldave2002@gmail.com>
2025-08-22 14:55:43 +0530

    feat: print matrix in spiral question

---
+++ arrays/medium/spiral-matrix.js
@@ -0,0 +1,45 @@
+function spiralMatrix(matrix) {
+  const n = matrix.length;
+  const m = matrix[0].length;
+
+  let top = 0,
+    left = 0,
+    bottom = n - 1;
+  right = m - 1;
+
+  const ans = [];
+
+  while (top <= bottom && left <= right) {
+    for (let i = left; i <= right; i++) {
+      ans.push(matrix[top][i]);
+    }
+    top++;
+
+    for (let i = top; i <= bottom; i++) {
+      ans.push(matrix[i][right]);
+    }
+    right--;
+
+    if (top <= bottom) {
+      for (let i = right; i >= left; i--) {
+        ans.push(matrix[bottom][i]);
+      }
+      bottom--;
+    }
+
+    if (left <= right) {
+      for (let i = bottom; i >= top; i--) {
+        ans.push(matrix[i][left]);
+      }
+      left++;
+    }
+  }
+
+  return ans;
+}
+
+spiralMatrix([
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+]);
