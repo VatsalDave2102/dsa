f1875d6788dd78147b8a98c156d6ad3518c5c3fe
Vatsal Dave <vatsaldave2002@gmail.com>
2025-08-25 15:47:04 +0530

    feat: add subarray sum equals K question

--- arrays/medium/maximum-subarray-sum.js
+++ arrays/medium/maximum-subarray-sum.js
@@ -24,7 +24,7 @@ function maximumSubarraySum(nums) {
   return maximum;
 }
 
-console.log(maximumSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+// console.log(maximumSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 
 function maximumSubarraySumWithArray(nums) {
   let maximum = Number.MIN_SAFE_INTEGER;
@@ -50,4 +50,41 @@ function maximumSubarraySumWithArray(nums) {
   return [...nums.slice(indexes[0], indexes[indexes.length - 1])];
 }
 
-console.log(maximumSubarraySumWithArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+// console.log(maximumSubarraySumWithArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+/**
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns return the number of contiguous subarrays in an array that have a sum equal to target k
+ */
+function countSubarraySumEqualsK(nums, k) {
+  // initialize array length, count and prefix sum
+  let n = nums.length;
+  let count = 0,
+    preSum = 0;
+
+  // initialize a map
+  const map = new Map();
+
+  // set default value
+  map.set(0, 1);
+
+  // iterate through array
+  for (let i = 0; i < n; i++) {
+    // store prefix sum
+    preSum += nums[i];
+
+    // calculate prefix sum - target
+    let remove = preSum - k;
+
+    // update count if remove is present in map i.e. a part of array already has the target value.
+    count += map.get(remove) ?? 0;
+
+    // set prefix sum as key and update the counter of that
+    map.set(preSum, (map.get(preSum) ?? 0) + 1);
+  }
+  return count;
+}
+
+console.log(countSubarraySumEqualsK([3, 1, 2, 4], 6));
